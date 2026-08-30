80d75274bd6fe1355c54163bd237c72084445f8a
yellow-man <84687741+Eric345-cyber@users.noreply.github.com>
2026-08-30 10:35:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: true // This tells Vite to allow Railway's public domains
+  }
 })
